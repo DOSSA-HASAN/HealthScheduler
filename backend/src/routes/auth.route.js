5aa8e458a562cd8f2952926451ee7fd4bad3f47b
Hasan <hassandossa5@gmail.com>
2025-04-19 16:51:36 +0300

    route for only admin to create doctor/admin account

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { createAccount, login, logout } from "../controllers/auth.controller.js"
+import { createAccount, login, logout, createDoctorOrAdminAccount } from "../controllers/auth.controller.js"
+import { verifyAdmin } from "../middleware/verifyAdmin.js"
 
 const router = express.Router()
 router.post('/login', login)
 router.post('/register', createAccount)
+router.post('/create', verifyAdmin, createDoctorOrAdminAccount)
 router.post('/logout', logout)
 
 export default router
