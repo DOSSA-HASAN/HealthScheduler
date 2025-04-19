d1118c49cc6dea6eb6e6094cf94d1991b770f526
Hasan <hassandossa5@gmail.com>
2025-04-19 17:17:32 +0300

    middleware to logout

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { createAccount, login, logout, createDoctorOrAdminAccount } from "../controllers/auth.controller.js"
 import { verifyAdmin } from "../middleware/verifyAdmin.js"
+import { verifyUser } from "../middleware/verifyUser.middleware.js"
 
 const router = express.Router()
 router.post('/login', login)
 router.post('/register', createAccount)
 router.post('/create', verifyAdmin, createDoctorOrAdminAccount)
-router.post('/logout', logout)
+router.post('/logout', verifyUser, logout)
 
 export default router
