9f310c950e92d83871bfd339cedc5a3733718fc0
Hasan <hassandossa5@gmail.com>
2025-04-19 16:49:50 +0300

    function to verify admin for protected route

---
+++ backend/src/middleware/verifyAdmin.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken"
+import { userModel } from "../models/userModel.js"
+import mongoose from "mongoose"
+
+export const verifyAdmin = async (req, res, next) => {
+    const token = req.cookies.token
+    if (!token)
+        return res.status(401).json({ message: "no token found" })
+
+    try {
+        const decodedAdmin = jwt.verify(token, process.env.SECRET_KEY)
+        let adminIdObject;
+        if(mongoose.isObjectIdOrHexString(decodedAdmin.id)){
+            adminIdObject = mongoose.Types.ObjectId.createFromHexString(decodedAdmin.id)
+        }
+
+        const admin = await userModel.findOne({ _id: adminIdObject, role: "admin" })
+        if (!admin)
+            return res.status(401).json({ message: "user is not an admin unauthorized person" })
+        req.user = admin
+        next()
+    } catch (error) {
+        console.log(`Error occured while verifying admin: ${error.message}`)
+        console.log(`Error occured while verifying admin: ${error.stack}`)
+        return res.status(500).json({ message: error.message })
+    }
+}
