528856acd0e949fa66e994fc8d75dc4ae8ba7047
Hasan <hassandossa5@gmail.com>
2025-04-19 16:59:59 +0300

    function to ensure users are logged in

---
+++ backend/src/middleware/verifyUser.middleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken"
+import { userModel } from "../models/userModel.js"
+
+export const verifyUser = async (req, res, next) => {
+    const token = req.cookies.token
+    if (!token)
+        return res.status(404).json({ message: "No token found" })
+
+    try {
+        const decodedUser = jwt.verify(token, process.env.SECRET_KEY)
+        if (!decodedUser)
+            return res.status().json({ message: "User could not be decoded" })
+
+        // find user from user model with corresponding payload id form decode
+        const user = await userModel.findById(decodedUser.id)
+        if (!user)
+            return res.status(404).json({ message: "No valid user found" })
+
+        req.user = user
+        next()
+    } catch (error) {
+        console.log("Error in verifyUser middleware " + error.message)
+        console.log("Error in verifyUser middleware " + error.stack)
+        return res.status(500).json({ message: error.message })
+    }
+}
