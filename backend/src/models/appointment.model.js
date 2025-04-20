daeb7e1e9b6ed531146fdd1dcf4deda6c238e0be
Hasan <hassandossa5@gmail.com>
2025-04-20 17:44:45 +0300

    added virtual fields

--- backend/src/models/appointment.model.js
+++ backend/src/models/appointment.model.js
@@ -6,11 +6,29 @@ const appointmentSchema = new mongoose.Schema(
         patientId: { type: mongoose.Types.ObjectId, required: true, ref: "user" },
         appointmentDate: { type: Date, required: true },
         status: { type: String, enum: ["pending", "confirmed", "completed", "cancelled"], default: "pending" },
-        notes: { type: String, default: "No prescription provided by doctor"}
+        notes: { type: String, default: "No prescription provided by doctor" }
     },
     {
-        timestamps: true
+        timestamps: true,
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
     }
 )
+
+appointmentSchema.virtual("doctor", {
+    ref: "user",
+    localField: "doctorId",
+    foreignField: "_id",
+    justOne: true,
+    options: { select: "-password" }
+})
+
+appointmentSchema.virtual("patient", {
+    ref: "user",
+    localField: "patientId",
+    foreignField: "_id",
+    justOne: true,
+    options: { select: "-password" }
+})
 
 export const appointmentForPateints = mongoose.model("appointment", appointmentSchema)
