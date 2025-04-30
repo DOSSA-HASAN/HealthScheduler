07d4c6c4f4433e0c8cbd49c0053ad33b18fee44f
Hasan <hassandossa5@gmail.com>
2025-04-30 22:19:48 +0300

    animation variants for framer motion

---
+++ frontend/src/lib/AnimationVariants.jsx
@@ -0,0 +1,7 @@
+import { animate } from "framer-motion";
+
+export const AnimationVariants = {
+    initial: { opacity: 0, y: 10 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -10 },
+}
