1e43db0221e5fc71369389336158951cd7dbda58
Hasan <hassandossa5@gmail.com>
2025-05-01 21:22:10 +0300

    auth route for google authentication

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { createAccount, login, logout, createDoctorOrAdminAccount } from "../controllers/auth.controller.js"
+import { createAccount, login, logout, createDoctorOrAdminAccount, googleLogin } from "../controllers/auth.controller.js"
 import { verifyAdmin } from "../middleware/verifyAdmin.js"
 import { verifyUser } from "../middleware/verifyUser.middleware.js"
 
 const router = express.Router()
+router.post('/google-login', googleLogin)
 router.post('/login', login)
 router.post('/register', createAccount)
 router.post('/create', verifyAdmin, createDoctorOrAdminAccount)
