561e035b114a597d18030c167f0008f6647b14af
Hasan <hassandossa5@gmail.com>
2025-05-02 11:37:02 +0300

    added fixes for production

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"
 
-const URL = 'http://localhost:5123'
+const URL = import.meta.env.MODE === "development" ? 'http://localhost:5123/api' : '/'
 
 export const socket = io(URL, { withCredentials: true, transports: ["websocket"] })
 
